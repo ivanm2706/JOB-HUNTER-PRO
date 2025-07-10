a11bee70ca92f42bc7e7cd6d6c7dc702e981ed43
Ivan Morhun <ivanmorhun.dev@gmail.com>
2025-07-09 20:31:11 -0500

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // отключает правила ESLint, конфликтующие с Prettier
+  ],
+  plugins: ['react', 'jsx-a11y', 'import', '@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    'eol-last': ['error', 'always'], // 👈 авто-перенос строки в конце
+    'react/react-in-jsx-scope': 'off', // для React 17+
+    'import/order': ['warn', { groups: [['builtin', 'external', 'internal']] }],
+  },
+};
